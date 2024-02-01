3263d9e908c2b1025d5a3e7a6097034510ae1644
AltayTaneri <taneri862@gmail.com>
2024-02-01 21:30:10 +0300

    added new state

--- pages/eventDetail.tsx
+++ pages/eventDetail.tsx
@@ -1,14 +1,15 @@
 import Footer from '@/components/FooterComponents/footer';
 import Navbar from '@/components/NavbarComponents/navbar';
-import React from 'react';
+import React, { useState } from 'react';
 import "../src/app/globals.css";
 import DetailHero from '@/components/EventDetailComponents/detailHero';
 import eventPhoto from '../src/assets/eventPhoto.png';
 
 const EventDetail: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className='flex flex-col items-center'>
-        <Navbar />
+        <Navbar isOpen={isOpen} setIsOpen={setIsOpen}/>
         <DetailHero image={eventPhoto}/>
         <Footer />
     </div>
