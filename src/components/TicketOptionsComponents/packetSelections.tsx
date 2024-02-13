9151546a509a411263b8aa17bc6f790fa2270033
AltayTaneri <taneri862@gmail.com>
2024-02-14 02:08:03 +0300

    updated card

--- src/components/TicketOptionsComponents/packetSelections.tsx
+++ src/components/TicketOptionsComponents/packetSelections.tsx
@@ -11,15 +11,22 @@ type Props = {
 
 const PacketSelections = (props: Props) => {
     const [count, setCount] = useState(0);
+
+    const updateCount = (data: number) => {
+        if(count >= 0 && data >= 0) {
+            setCount(data);
+        }
+    };
+
   return (
-    <div className='flex flex-col items-center justify-center p-4 gap-2'>
-        <h2 className={`${cabinetGrostek.className} text-2xl text-[#1b1b25] font-bold text-center`}>Paket ${props.title}</h2>
+    <div className='flex flex-col items-center justify-center p-4 gap-2 border-[1px] border-[#828282] rounded max-w-72'>
+        <h2 className={`${cabinetGrostek.className} text-2xl text-[#1b1b25] font-bold text-center`}>{props.title}</h2>
         <p className={`${generalSans.className} text-sm text-[#1b1b25] font-normal text-center`}>{props.data}</p>
         <p className={`${generalSans.className} text-2xl text-[#1b1b25] font-medium text-center`}>Rp. {props.price}</p>
         <div className='flex items-center justify-center gap-12'>
-            <PacketButton image={Minus} function={() => setCount(count - 1)}/>
+            <PacketButton image={Minus} function={() => updateCount(count - 1)}/>
             <p className='text-black'>{count}</p>
-            <PacketButton image={Plus} function={() => setCount(count + 1)}/>
+            <PacketButton image={Plus} function={() => updateCount(count + 1)}/>
         </div>
     </div>
   )
