0be27819198d1a3b00210d5f107ff30f28ebf8f8
battletech45 <taneri862@gmail.com>
2024-02-27 14:15:46 +0300

    updated css for mobile rev2

--- src/components/TicketOptionsComponents/TicketBackHeader.tsx
+++ src/components/TicketOptionsComponents/TicketBackHeader.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const TicketBackHeader = (props: Props) => {
   return (
-    <div className='flex items-center justify-start gap-12'>
+    <div className='flex items-center justify-center md:justify-start gap-12'>
         <div className='flex items-center justify-center'>
             <ArrowLeft color={'#4f4cee'} size={40} className='border-2 rounded border-[#4f4cee] p-1'/>
         </div>
